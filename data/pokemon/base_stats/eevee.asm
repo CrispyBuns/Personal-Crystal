59aa570d573d0850ce7f5e9181b05c2d5eaf686d
CrispyBuns <Crispybuns2003@gmail.com>
2026-05-09 17:28:02 -0500

    Make eevee hatch very fast

--- data/pokemon/base_stats/eevee.asm
+++ data/pokemon/base_stats/eevee.asm
@@ -1,11 +1,11 @@
-	db  55,  55,  50,  55,  45,  65 ; 325 BST
+db  55,  55,  50,  55,  45,  65 ; 325 BST
 	;   hp  atk  def  spe  sat  sdf
 
 	db NORMAL, NORMAL ; type
 	db 45 ; catch rate
 	db 92 ; base exp
 	db NO_ITEM, NO_ITEM ; held items
-	dn GENDER_F25, HATCH_MEDIUM_FAST ; gender ratio, step cycles to hatch
+	dn GENDER_F25, HATCH_FASTEST ; gender ratio, step cycles to hatch
 
 	abilities_for EEVEE, RUN_AWAY, ADAPTABILITY, ANTICIPATION
 	db GROWTH_MEDIUM_FAST ; growth rate
