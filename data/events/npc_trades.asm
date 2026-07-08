19238004ac93fe4586cf8175cbbdf8af88553895
CrispyBuns <Crispybuns2003@gmail.com>
2026-07-08 11:48:10 -0500

    Trainer rename

--- data/events/npc_trades.asm
+++ data/events/npc_trades.asm
@@ -7,7 +7,7 @@ NPCTrades:
 	rawchar "Scizzorman@", $00
 	db $FF, $FF, $FF, SHINY_MASK | HIDDEN_ABILITY | NAT_ATK_UP_SATK_DOWN,  LUXURY_BALL,   METAL_COAT
 	dw 92656
-	rawchar "Mikey@@", $00
+	rawchar "Michael", $00
 ; NPC_TRADE_KYLE in Violet City
 	db TRADE_DIALOGSET_COLLECTOR
 	dp SENTRET, NO_FORM ; wants
